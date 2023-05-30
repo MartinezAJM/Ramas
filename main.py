da1fb744e071f7db28d6d16e0fe9de277fa9df2a
MartinezAguilera <martinezj85@uabc.edu.mx>
2023-05-29 20:39:01 -0700

    Agregue la resta al menu

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ def accion1():
     print('Has elegido la opción 1')
 
 
-def accion2():
+def resta():
     print('Has elegido la opción 2')
 
 def salir():
     print('Salir')
+
+def resta():
+    a = 100
+    b = 75
+    resta= a - b
+    print("La resta de dos numeros asignados es :" + resta)
