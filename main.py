f3ce5fc3764cdcbc07acc9cfa3b73e89b6782e27
MartinezAguilera <martinezj85@uabc.edu.mx>
2023-05-29 20:36:59 -0700

    Agregue una funcion en el menu

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
-def accion1():
-    print('Has elegido la opción 1')
+def accion5():
+    print('Has elegido la opción 5')
 
 
-def accion2():
-    print('Has elegido la opción 2')
-
+def suma():
+    print('Has elegido la opción 4')
 
 def accion3():
     print('Has elegido la opción 3')
 
+def accion2():
+    print('Has elegido la opción 2')
+
+def accion1():
+    print('Has elegido la opción 1')
 
 def salir():
     print('Saliendo')
+
+def suma():
+    a= 35
+    b= 45
+    suma = a + b
+    print("La suma de dos numero es :" + suma)
