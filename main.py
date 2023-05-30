965017c5fcd7fc84b832093bf11cab34a193f4cb
MartinezAguilera <martinezj85@uabc.edu.mx>
2023-05-29 20:17:30 -0700

    Agregue el main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+def accion1():
+    print('Has elegido la opción 1')
+
+
+def accion2():
+    print('Has elegido la opción 2')
+
+
+def accion3():
+    print('Has elegido la opción 3')
+
+
+def salir():
+    print('Saliendo')
